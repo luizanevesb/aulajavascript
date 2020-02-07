a9adb29ba15de3b0de5b23caec80b7919136f564
Luiza <luiza.nbs@gmail.com>
2020-02-07 20:33:29 -0300

    bloqueio de teclado

--- alunos.js
+++ alunos.js
@@ -1,51 +1,104 @@
+
 $(document).ready(function(){
 
-    $("#btn-cadastrar").click(function (){
+    $("#btn-cadastrar").click(function () {
+
+        var vetor_periodos = ["", "Manhã", "Tarde", "Noite"];
 
         var nome = $("#nome").val();
         var email = $("#email").val();
         var telefone = $("#telefone").val();
+        var periodo = $("#periodo").val()
         var sexo = $("input[name=sexo]:checked").val();
-        var periodo = $("#periodo").val();
 
-        if (nome == "") 
-        {
-            alert("O campo nome é obrigatório.");
+        if (nome.trim().length < 2)
+        {            
+            alert("O campo nome é obrigatorio");
+            $("#nome").focus();
             return false;
         }
 
-        var linha = "<tr><td>" + nome + "</td>"
-        +"<td>" + email + "</td>"
-        +"<td>" + telefone + "</td>"
-        +"<td>" + sexo + "</td>"
-        +"<td>" + periodo + "</td></tr>";
+        if (email == "")
+        {
+            alert("O campo email é obrigatorio");
+            $("#email").focus();
+            return false;
+        } else 
+        {
+            var email_regex = /^[0-9a-z\.\_\-]*@[0-9a-z.]*$/gm;
+            if (email.search(email_regex) == -1)
+            {
+                alert("O formato do email é invalido");
+                $("#email").focus();
+                return false;
+            }
+        }
+
+        if (telefone == "")
+        {
+            alert("O campo telefone é obrigatorio");
+            $("#telefone").focus();
+            return false;
+        } else 
+        {
+            var fone_regex = /^\([0-9][0-9]\)[0-9]{4,5}-[0-9]{4}$/gm;
+            
+            if (telefone.search(fone_regex) == -1)
+            {
+                alert("O formato do telefone é invalido");
+                $("#telefone").focus();
+                return false;
+            }
+
+        }
+
+        if (sexo == undefined)
+        {
+            alert("O campo sexo é obrigatorio");            
+            return false;
+        }
+
+        if (periodo == 0)
+        {
+            alert("O campo periodo é obrigatorio");
+            $("#periodo").focus();
+            return false;
+        }
+
+        /*
+        if (sexo == "M")
+        {
+            var sexo_rotulo = "Masculino";
+        } else {
+            var sexo_rotulo = "Feminino";
+        }*/
+        // if ternario
+        var sexo_rotulo = (sexo == "M")? "Masculino" : "Feminino";
+
+        var linha = "<tr>"
+            +"<td>"+ nome +"</td>"
+            +"<td>"+ email +"</td>"
+            +"<td>"+ telefone +"</td>"
+            +"<td>"+ sexo_rotulo +"</td>"
+            +"<td>"+ vetor_periodos[periodo] + "</td>"
+            +"</tr>";
 
         $("#alunos").append(linha);
 
     }); //fim do click
 
-}); //fim ready
+    $("#telefone").keydown(function (ev){
+        console.log(ev);
 
-/*
-function cadastrar() 
-{
-    var nome = document.getElementById("nome").value;
-    var email = document.getElementByI("email").value;
-    var telefone = document.getElementByI("telefone").value;
+        if (ev.key.search(/[0-9()-]/g) != -1 || ev.keyCode == 8 || ev.keyCode == 9 || ev.keyCode == 46)
+        {
+            return true;
+        } else {
+            return false;
+        }
 
-    var alunos = $("alunos");
+    });
 
-    if (nome == "") 
-    {
-        alert("O campo nome é obrigatório.");
-        return false;
-    }
+}); // fim ready
 
-    alunos.innerHTML += "<tr><td>" + nome + "</td>"
-    +"<td>" + email + "</td>"
-    +"<td>" + telefone + "</td>"
-    +"<td>F</td>"
-    +"<td>Manhã</td></tr>";
-;
 
-}*/
