413e1a42d86248c2ea00048fa510621aba7cc639
Luiza <luiza.nbs@gmail.com>
2020-02-14 21:38:24 -0300

    aula vue

---
+++ api/index.js
@@ -0,0 +1,21 @@
+console.log("Servidor startado");
+
+var express = require('express');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res){
+    res.send("Ola");
+})
+
+app.get("/contato", function(req, res){
+    res.render('contato');
+})
+
+app.post("/contato", function(req, res){
+    res.send("form enviado");
+})
+
+app.listen(3000);
