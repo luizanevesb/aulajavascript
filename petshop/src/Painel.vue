2aa11b46dc221c9e5ea8b3b80e24aba772dbb913
Luiza <luiza.nbs@gmail.com>
2020-02-17 21:45:41 -0300

    aula vue loja pets

---
+++ petshop/src/Painel.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <h2>Novo cadastro para: {{ pet }}</h2> 
+    <fieldset>
+
+        <label>Nome do Pet:</label>
+        <input type="text" v-model="pet"/> <br>
+        <label>Dono:</label>
+        <input type="text" v-model="dono"/> <br>
+        <button @click="cadastro">Cadastrar</button>
+
+    </fieldset>    
+
+    <table>
+        <thead>
+            <tr>
+                <th>Pet</th>
+                <th>Dono</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(i, idx) in lista" :key="idx">
+                <td>{{ i.pet }}</td>
+                <td>{{ i.dono }}</td>
+                <td @click="excluir(idx)" class="deletar">[X]</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+</template>
+
+<script>
+export default {
+    data: function()
+    {
+        return {
+            lista: [
+                {"pet": "Bob", "dono": "João"}, 
+                {"pet": "Lulu", "dono": "Maria"},
+                {"pet": "Amy", "dono": "Luiza"}
+            ],
+            pet: "",
+            dono: ""
+        }     
+    },
+    methods: {
+        cadastro: function() {
+            var item = {
+                "pet": this.pet, "dono": this.dono
+            }
+            this.lista.push(item);
+            this.pet = "";
+            this.dono = "";
+        },
+        excluir: function(idx) {
+            
+            this.lista.splice(idx, 1);
+        }
+    }
+}
+</script>
+
+<style>
+    label {
+        width: 100px;
+        display: inline-block;
+        text-align: right;
+        font-size: small;
+    }
+    fieldset {
+        width: 50%;
+    }
+    table {
+        margin-top: 15px;
+        border: 1px dotted;
+        border-collapse:separate;
+        width: 50%;
+    }
+    th, td {
+        border: 1px dotted;
+        padding: 4px;
+    }
+    thead {
+        background-color:lightpink;
+    }
+    .deletar {
+        color: crimson;
+        cursor: pointer;
+    }
+</style>
---
+++ petshop/src/main.js
@@ -0,0 +1,12 @@
+//ES6 - importando libs
+import Vue from 'vue'
+//import App from './App.vue'
+import Painel from './Painel.vue'
+
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(Painel),
+}).$mount('#app')
+
