f3c009e9aae409e2ac7a2d9df2ed929e37e4b02f
Luiza <luiza.nbs@gmail.com>
2020-02-18 21:54:50 -0300

    aula vue banho e tosa

---
+++ banhotosa/src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="app">
+    <b-navbar type="dark" variant="success">
+      <b-navbar-brand >Banho e Tosa</b-navbar-brand>
+
+      <b-navbar-nav>
+        <b-nav-item href="#" @click="exibeListagem = false">Agendar</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+
+
+    <b-container>
+      <b-row>
+        <div v-if="exibeListagem">
+        <b-card title="Agendamentos" class="col-md-12">
+            <b-table striped hover :items="agendamentos" :fields="colunas">
+
+            </b-table>
+        </b-card>
+        </div>
+        <div v-else>
+          <Agendamento></Agendamento>
+        </div>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+
+import Agendamento from "./components/Agendamento"
+
+export default {
+  name: 'App',
+
+  components:{Agendamento},
+
+  data()
+  {
+    return{
+      exibeListagem: true,
+      colunas: [
+        {
+          key:"datahora", label:"Data/Hora", sortable: true
+        },
+        {
+          key:"pet", label:"Nome do Pet", sortable: true
+        },
+        {
+          key:"servico",
+          label:"Serviço"
+        }
+        
+      ],
+      agendamentos: [
+        {"datahora":"10-10-2020 10:10", "pet":"Amy", "servico":"banho"},
+        {"datahora":"10-10-2020 10:30", "pet":"Rex", "servico":"tosa"}
+      ]
+      
+
+    }
+   
+  }
+
+}
+</script>
+
+<style>
+</style>
---
+++ banhotosa/src/components/Agendamento.vue
@@ -0,0 +1,48 @@
+<template>
+  <b-card title="Novo serviço" class="col-md-12">
+    <b-form>
+        <b-form-group label="Data e Hora">
+        <b-form-datepicker id="datahora"></b-form-datepicker>
+        </b-form-group>
+        <b-form-group label="Nome do Pet">
+        <b-form-input id="pet"></b-form-input>
+        </b-form-group>
+        <b-form-group label="Nome do Dono">
+        <b-form-input id="dono"></b-form-input>
+        </b-form-group>
+        <b-form-group label="Serviço">
+        <b-form-select :options="servicos"></b-form-select>
+        </b-form-group>
+        <b-form-group label="Telefone">
+        <b-form-input id="telefone"></b-form-input>
+        </b-form-group>
+
+        <b-button variant="success" @click="cadastrar">Enviar</b-button>
+        <b-button variant="danger" @click="$parent.exibeListagem = true">Cancelar</b-button>
+    </b-form>
+  </b-card>
+</template>
+
+<script>
+export default {
+
+    name: "Agendamento",
+
+    data(){
+        return {
+            servicos: ["Banho","Tosa","Consulta"]
+        }
+    },
+    methods: {
+      cadastrar()
+      {
+
+      }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
